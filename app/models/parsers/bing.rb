9e2b42c8ead9de7ecca3101039c898d80f8f719b
ignoch <ignoch@gmail.com>
2021-04-15 07:22:49 -0500

    refactoring parsers

--- app/models/parsers/bing.rb
+++ app/models/parsers/bing.rb
@@ -1,37 +1,15 @@
 module Parsers
-  class Bing
-    attr_accessor :response, :context
+  class Bing < Base
 
-    def initialize(engine)
-      @response = engine.search
-      @context = Context.new
+    def data_root(body)
+      body["webPages"]["value"]
     end
 
-    def results
-      process
-      context
-    end
-
-    private
-
-    def process
-      return context.fail!(response_error) unless valid?
-
-      body = response.parsed_response
-      context.collection = body["webPages"]["value"].map do |value|
-        {
-          name: value["name"],
-          url: value["url"]
-        }
-      end
-    end
-
-    def valid?
-      response.code == 200
-    end
-
-    def response_error
-      response.parsed_response["error"]["message"]
+    def data_map(node)
+      {
+        name: node["name"],
+        url: node["url"]
+      }
     end
   end
 end
--- app/models/parsers/google.rb
+++ app/models/parsers/google.rb
@@ -1,37 +1,14 @@
 module Parsers
-  class Google
-    attr_accessor :response, :context
-
-    def initialize(engine)
-      @response = engine.search
-      @context = Context.new
+  class Google < Base
+    def data_root(body)
+      body["items"]
     end
 
-    def results
-      process
-      context
-    end
-
-    private
-
-    def process
-      return context.fail!(response_error) unless valid?
-
-      body = response.parsed_response
-      context.collection = body["items"].map do |value|
-        {
-          name: value["title"],
-          url: value["link"]
-        }
-      end
-    end
-
-    def valid?
-      response.code == 200
-    end
-
-    def response_error
-      response.parsed_response["error"]["message"]
+    def data_map(node)
+      {
+        name: node["title"],
+        url: node["link"]
+      }
     end
   end
 end
