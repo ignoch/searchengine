246c5f68e0bf735824ee999aa253c79fa3bd67a3
ignoch <ignoch@gmail.com>
2021-04-20 20:49:26 -0500

    refactoring bing service

---
+++ app/services/search_api/bing/search_service.rb
@@ -0,0 +1,27 @@
+class SearchApi::Bing::SearchService < ApplicationService
+  def initialize(search_string, options)
+    @search_string = URI.encode_www_form_component(search_string)
+    @options = options
+  end
+
+  def call
+    result = SearchApi::Bing::QueryService.call(search_string, options)
+    if result.success?
+      OpenStruct.new(success?: true, collection: parse_data(result.collection))
+    else
+      OpenStruct.new(success?: false, error: result.error)
+    end
+  end
+
+  private
+  attr_reader :search_string, :options
+
+  def parse_data(collection)
+    collection.map do |data|
+      {
+        name: data["name"],
+        url: data["url"]
+      }
+    end
+  end
+end
