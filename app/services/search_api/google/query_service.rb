748dae51757b1606024601c2ffb4abac5f265425
ignoch <ignoch@gmail.com>
2021-04-20 21:57:15 -0500

    refactor google search into service

---
+++ app/services/search_api/google/query_service.rb
@@ -0,0 +1,53 @@
+class SearchApi::Google::QueryService < ApplicationService
+  include HTTParty
+  base_uri 'https://www.googleapis.com/customsearch'
+
+  def initialize(search, params={})
+    @search = search
+    offset = params.delete("offset") { 1 }
+    params = params.merge(auth_query_params)
+
+    @options = { query: params.merge({
+      q: search, start: offset
+    })}
+  end
+
+  def call
+    response = self.class.get('/v1', options)
+    if success?(response)
+      OpenStruct.new(success?: true, collection: parse_data(response))
+    else
+      OpenStruct.new(success?: false, error: parse_error(response))
+    end
+  end
+
+  private
+  attr_reader :options, :params
+
+  def auth_query_params
+    {
+      key: api_key,
+      cx: cx_code
+    }
+  end
+
+  def success?(response)
+    response.code == 200
+  end
+
+  def parse_error(response)
+    JSON.parse(response.body)["error"]["message"]
+  end
+
+  def parse_data(response)
+    response.parsed_response["items"]
+  end
+
+  def api_key
+    ENV['GOOGLE_API_KEY'].to_s
+  end
+
+  def cx_code
+    ENV['GOOGLE_CX_CODE'].to_s
+  end
+end
